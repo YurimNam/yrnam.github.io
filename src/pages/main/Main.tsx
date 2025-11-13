7ec7082600f91915686af01993cd76235b1c9a35
Yurimnam <makodbfla1@gmail.com>
2025-11-13 15:16:38 +0900

    feat: 원페이지 스크롤 기능 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -6,15 +6,40 @@ export function Main() {
   const scroll = (e: WheelEvent) => {
     if (!elementRef.current) return;
 
+    e.preventDefault();
+
     console.log(elementRef.current.scrollTop);
 
     const pageHeight = window.innerHeight;
-    console.log(pageHeight, e.deltaY);
-    // 아래로 스크롤
+    const scrollTop = elementRef.current.scrollTop;
+    const isFirstPage = scrollTop < pageHeight;
+    const isSecondPage = scrollTop >= pageHeight && scrollTop < pageHeight * 2;
+    const isThirdPage =
+      scrollTop >= pageHeight * 2 && scrollTop < pageHeight * 3;
+    const isLastPage =
+      scrollTop >= pageHeight * 3 && scrollTop < pageHeight * 4;
+
+    let movePageHeight = 0;
+
     if (e.deltaY > 0) {
-      // 위로 스크롤
+      if (isFirstPage) movePageHeight = pageHeight;
+
+      if (isSecondPage) movePageHeight = pageHeight * 2;
+
+      if (isThirdPage) movePageHeight = pageHeight * 3;
     } else {
+      if (isSecondPage) movePageHeight = 0;
+
+      if (isThirdPage) movePageHeight = pageHeight;
+
+      if (isLastPage) movePageHeight = pageHeight * 2;
     }
+
+    elementRef.current.scrollTo({
+      top: movePageHeight,
+      left: 0,
+      behavior: "smooth"
+    });
   };
 
   useEffect(() => {
